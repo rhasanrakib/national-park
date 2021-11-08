ea375253e27dd78320f7fd9ae84bcf753de23569
Rakib <rakibulh170@gmail.com>
2021-11-08 23:08:24 +0600

    new design

--- app.js
+++ app.js
@@ -1,97 +1,118 @@
-const searchBtn = document.getElementById('search-btn');
-const mealList = document.getElementById('meal');
-const mealDetailsContent = document.querySelector('.meal-details-content');
-searchBtn.addEventListener('click', getMealList);
-mealList.addEventListener('click', getMealRecipe);
-function btnClose() {
-    document.getElementById("hide-show").style.display="none";
-    document.getElementById("wrapper").style.display="block"; 
-}
-// get meal list 
-let queryParams={
-    api_key:'j8lMm6FaFcghlNpqXfi3Fq6rbFX0TZt4YzBKxOZw',
-}
-let baseUrl='https://developer.nps.gov/api/v1/'
-function getMealList() {
-    let searchInputTxt = document.getElementById('search-input').value.trim();
-    if(searchInputTxt!=""){
-        queryParams={
-            ...queryParams,
-            ...{q:searchInputTxt}
-        }
+let baseUrl = "https://developer.nps.gov/api/v1/";
+
+const searchSongs = () => {
+  const searchText = document.getElementById("search-field").value.trim();
+
+  queryParams = {
+    api_key: "j8lMm6FaFcghlNpqXfi3Fq6rbFX0TZt4YzBKxOZw",
+    ...(searchText != "" && { q: searchText }),
+  };
+  console.log(queryParams);
+  // load data
+  const url = `${baseUrl}activities?${new URLSearchParams(queryParams)}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displaySongs(data.data))
+    .catch((error) =>
+      displayError("Something Went Wrong!! Please try again later!")
+    );
+};
+
+const displaySongs = (songs) => {
+  const songContainer = document.getElementById("song-container");
+  songContainer.innerHTML = "";
+
+  songs.forEach((song) => {
+    const songDiv = document.createElement("div");
+    songDiv.className = "single-result row align-items-center my-3 p-3";
+
+    songDiv.innerHTML = `
+        <div class="col-md-9">
+            <h3 class="lyrics-name">${song.name}</h3>
+            
+           
+        </div>
+        <div class="col-md-3 text-md-right text-center">
+            <button onclick="getLyric('${song.id}')" class="btn btn-success">Get Lyrics</button>
+        </div>
+        `;
+
+    songContainer.appendChild(songDiv);
+  });
+};
+
+const getLyric = async (artist) => {
+  const url = `${baseUrl}activities/parks?${new URLSearchParams({
+    api_key: "j8lMm6FaFcghlNpqXfi3Fq6rbFX0TZt4YzBKxOZw",
+    id: artist,
+  })}`;
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    displayLyrics(data.data);
+  } catch (error) {
+    displayError("Sorry! I failed to load lyrics, Please try again later!!!");
+  }
+};
+
+
+
+const displayLyrics = async(lyrics) => {
+    park = lyrics[0].parks
+    //console.log(park)
+    let html='';
+    for (let i of park) {
+        html += `
+        <a herf="#" onclick="getParkDetails('${i.parkCode}')">
+            <h2>${i.fullName}</h2>
+        </a>
+        `;
+
     }
-    console.log(queryParams)
-    fetch(`${baseUrl}activities?${new URLSearchParams(
-        queryParams
-    )}`)
-        .then(response => response.json())
-        .then(data => {
-            let html = '';
-            if (data.data) {
-                
-                data.data.forEach(elem => {
-                    html += `
-                <div class="meal-item" id = "${elem.id}">
-                        
-                        <div class="meal-name">
-                            <h3>${elem.name}</h3>
-                            <a class="recipe-btn" src="#">Enter</a>
-                        </div>
-                    </div>
-                `
-                });
-                mealList.classList.remove('notFound');
-            }
-            else {
-                html = "Sorry, we didn't find any meal!!";
-                mealList.classList.add('notFound');
-            }
-            mealList.innerHTML = html;
-        })
+
+  const lyricsDiv = document.getElementById("song-lyrics");
+  lyricsDiv.innerHTML = html;
+};
+
+const displayError = (error) => {
+  const errorTag = document.getElementById("error-message");
+  errorTag.innerText = error;
+};
+
+const getParkDetails=async (parkCode)=>{
+    const url = `${baseUrl}parks?${new URLSearchParams({
+        api_key: "j8lMm6FaFcghlNpqXfi3Fq6rbFX0TZt4YzBKxOZw",
+        parkCode: parkCode,
+      })}`;
+      try {
+        const res = await fetch(url);
+        const data = await res.json();
+        displayParksDetails(data.data[0]);
+      } catch (error) {
+        displayError("Sorry! I failed to load lyrics, Please try again later!!!");
+      }
 }
 
-// get meal recipe
-function getMealRecipe(e) {
-    //console.log(e)
-    document.getElementById("hide-show").style.display="block";
-    document.getElementById("wrapper").style.display="none";
-    //console.log("here");
-    e.preventDefault();
+const displayParksDetails = async(parks) => {
     
-    if (e.target.classList.contains('recipe-btn')) {
-        let mealItem = e.target.parentElement.parentElement;
-        //console.log(`${baseUrl}activities/parks?${new URLSearchParams({...queryParams,...{id:mealItem.id}})}`)
-        fetch(`${baseUrl}activities/parks?${new URLSearchParams({...queryParams,...{id:mealItem.id}})}`)
-            .then(response => response.json())
-            .then(data => {
-                //console.log(data.data)
-                mealRecipeModal(data.data)
-            });
-    }
-}
-
-// create meals 
-function mealRecipeModal(park) {
-
- if(park){
-     park = park[0].parks;
- }
- let html='';
-
-    for(let i of park){
+    //console.log(parks)
+    let html='';
+    
         html += `
         
-        <a herf="#">
-        
-            <h2 class="recipe-title">${i.fullName}</h2>
-            <p class="recipe-category">${i.designation}
-            </p>
-        
-        </a>
-        
+            <h2>${parks.fullName}</h2>
+            <br>
+            <p>${parks.description}</p>
         
         `;
-        
-    }
-    document.getElementById("showRecipes").innerHTML = html;
-}
+
+    const phone=parks.contacts.phoneNumbers;
+    const emailAddresses = parks.contacts.emailAddresses;
+    const entranceFees = parks.entranceFees;
+
+    html+="directionsInfo: "+parks.directionsInfo;
+    html+='addresses: '+parks.addresses[0];
+
+  const lyricsDiv = document.getElementById("song-lyrics");
+  lyricsDiv.innerHTML = html;
+};
